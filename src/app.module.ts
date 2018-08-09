1a4b80b2adcd8241c1f1631a43ec5b0a9a743157
Manjula Dube <dube.manjula668@gmail.com>
2018-08-09 21:57:37 +0200

    some code added

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,11 @@ import { AppService } from './app.service';
 import { TweetsModule } from './tweets/tweets.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import expressPlayground from 'graphql-playground-middleware-express';
 
 // tslint:disable-next-line:no-var-requires
+const { graphqlExpress} = require('apollo-server-express');
+
 const { ApolloServer } = require('apollo-server');
 // tslint:disable-next-line:no-var-requires
 const { importSchema } = require('graphql-import');
@@ -37,23 +40,43 @@ export class AppModule {
 
   configure(consumer: MiddlewareConsumer) {
 
-    const typeDefs = importSchema(path.resolve('src/schema.graphql'));
+    const schema = this.createSchema();
+    
+    // const typeDefs = importSchema(path.resolve('src/schema.graphql'));
 
-    const server = new ApolloServer({
-        typeDefs,
-        context: req => ({
-          ...req,
-          prisma: new Prisma({
-            typeDefs: 'src/generated/prisma.graphql',
-            endpoint: process.env.PRISMA_URL,
-          }),
-        })});
+    // const server = new ApolloServer({
+    //     typeDefs,
+    //     context: req => ({
+    //       ...req,
+    //       prisma: new Prisma({
+    //         typeDefs: 'src/generated/prisma.graphql',
+    //         endpoint: process.env.PRISMA_URL,
+    //       }),
+    //     })});
 
     // this.initSubscriptionServer(schema);
     // this.subscriptionsService.createSubscriptionServer(schema);
 
     consumer
-      .apply(server)
+      .apply(
+        expressPlayground({ endpoint: '/graphql'}),
+      )
+      .forRoutes('/playground')
+      .apply(graphqlExpress({
+        schema,
+        context: req => ({
+          ...req,
+          prisma: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: process.env.PRISMA_URL,
+          })})}))
       .forRoutes('/graphql');
   }
+
+  createSchema() {
+    const typeDefs = this.graphQLFactory.mergeTypesByPaths('./schema/**/*.graphql');
+    return this.graphQLFactory.createSchema({ typeDefs  });
+  }
+
+   
 }
