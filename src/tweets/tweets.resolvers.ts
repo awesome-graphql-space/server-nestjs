c6cb2c1984646ed6c5127674065a0b1e5f6f3ad7
Rex Raphael <rex.raphael@gueva.org>
2018-08-09 19:03:30 +0100

    I am

--- src/tweets/tweets.resolvers.ts
+++ src/tweets/tweets.resolvers.ts
@@ -6,10 +6,30 @@ import {
   Subscription,
   ResolveProperty,
 } from '@nestjs/graphql';
-
+import { getUser, Context } from '../common/utils';
 import { TweetsService } from './tweets.service';
 
 @Resolver('Tweet')
 export class TweetsResolvers {
   constructor(private readonly userService: TweetsService) { }
+
+  @Mutation()
+  async post(obj, {text, upload}: any, ctx: Context, info): Promise<JwtAuthPayload | any> {
+    return null;
+  }
+
+  @Query()
+  async tweets(obj, {text, upload}: any, ctx: Context, info): Promise<JwtAuthPayload | any> {
+    return null;
+  }
+
+  @Query()
+  async myTweets(obj, {text, upload}: any, ctx: Context, info): Promise<JwtAuthPayload | any> {
+    return null;
+  }
+
+  @Query()
+  tweetSubscription (parent, args, ctx: Context, info) {
+    return ctx.db.subscription.tweet({}, info);
+  },
 }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -6,10 +6,15 @@ import {
   Subscription,
   ResolveProperty,
 } from '@nestjs/graphql';
-
+import { getUser, Context } from '../common/utils';
 import { UsersService } from './users.service';
 
 @Resolver('User')
 export class UsersResolvers {
   constructor(private readonly userService: UsersService) { }
+
+  @Query()
+  async currentUser(obj, {text, upload}: any, ctx: Context, info): Promise<JwtAuthPayload | any> {
+    return await getUser(ctx);
+  }
 }
