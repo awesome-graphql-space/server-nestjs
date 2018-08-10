0d7124a5c696471f92118b10487dc22e687148a3
Clayton Ray <iamclaytonray@gmail.com>
2018-08-10 12:20:45 -0400

    feat: setup basic middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
+import * as logger from 'morgan';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);   
+  app.use(cors());
+  app.use(helmet());
+  app.use(compression());
+  app.use(logger('dev'));
+  await app.listen(3000);
 }
 bootstrap();
