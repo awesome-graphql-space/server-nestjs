170a8748cf27d536eb620ef4e7ace52760228fad
Rex Raphael <rex.raphael@outlook.com>
2018-09-06 11:33:57 +0100

    first full working upgrade

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import * as path from 'path';
+import { importSchema } from 'graphql-import';
 import { Prisma } from './generated/prisma';
 import { resolvers } from './resolvers-orm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TweetsModule } from './tweets/tweets.module';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-
-// tslint:disable-next-line:no-var-requires
-const { ApolloServer } = require('apollo-server-express');
-// tslint:disable-next-line:no-var-requires
-const { importSchema } = require('graphql-import');
-
-// tslint:disable-next-line:no-var-requires
-const path = require('path');
 
 const db = new Prisma({
   endpoint: 'https://eu1.prisma.sh/nest/prisma-test2/dev', // the endpoint of the Prisma API (value set in `.env`)
@@ -26,9 +17,6 @@ const typeDefs = importSchema(path.resolve('src/schema.graphql'));
 
 @Module({
   imports: [
-    AuthModule,
-    TweetsModule,
-    UsersModule,
     GraphQLModule.forRoot({
         typeDefs,
         installSubscriptionHandlers: true,
